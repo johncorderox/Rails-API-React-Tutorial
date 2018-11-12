f1df7d49fcf2ffc6cd7e7c4006231d494a7d7bd9
John Cordero <johncorderox@gmail.com>
2018-11-12 13:07:10 -0800

    API

---
+++ test-api/config/initializers/cors.rb
@@ -0,0 +1,10 @@
+class Application < Rails::Application
+
+   config.middleware.insert_before 0, "Rack::Cors" do
+     allow do
+       origins '*'
+       resource '*', :headers => :any, :methods => [:get, :post, :patch, :options]
+     end
+   end
+
+ end
